2ebc25800ba6351573d4f996423465328290c9f0
Karim Semin <karim@wcw-stud-145-109-10-84.wireless.uva.nl>
2020-01-09 14:14:22 +0100

    structuur van de files aangepast. Classes verdeeld in aparte .py files (werkt nog niet)

---
+++ code/classes/car.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__(self, name, orientation, coordinates, length):
+        self.name = name
+        self.orientation = orientation
+        self.coordinates = coordinates
+        self.length = length
+        # print(f" {self.orientation}, {self.coordinates}, {self.length}")
+        print(f"Car {self.name}:{self.orientation}, {self.coordinates}, {self.length}"))
+
+    def move(self):
+            pass
+
+    def won(self):
+        pass
+        # if rode auto staat op plek 3,6 --> win
+
+    def __repr__(self):
+        return(f"{self.orientation}, {self.coordinates}, {self.length}")
---
+++ code/classes/game.py
@@ -0,0 +1,48 @@
+import csv
+from .car import Car
+from .board import Board
+
+
+# Mentorgesprek notes: class car (x,y, orientatie, length, name)
+# class board
+# `classs game (car_list, board)
+# functie Move_car(iD, direction), als deze wordt aangeroepen, zorg dan dat je game veranderds
+# functie Is_win
+# functie Is_emptyss
+# Mathplotlib gebruiken om vierkantjes te maken om bord te visualiseren `9makkelijker dan je denkt`
+# de algorithmes pakken move_cvar
+
+
+class Game:
+    def __init__(self, gameboard_file):
+        self.cars = self.load_cars(gameboard_file)
+
+    def load_cars(self, gameboard_file):
+        """
+        Loads all cars into the game
+        """
+        # read through csv gameboard file
+        with open(gameboard_file, "r") as in_file:
+            reader = csv.DictReader(in_file, skipinitialspace=True)
+            cars = []
+
+            # create car for each row
+            for row in reader:
+                car = Car(row['car'], row['orientation'], row['x,y'], row['length'])
+                cars.append(car)
+
+            return cars
+
+    # in string method functie maken die bord simuleert
+    def __repr__(self):
+        s=""
+        for c in self.cars:
+            s+=str(c)
+        return s
+        #return(f"Auto's:{self.cars}")
+
+
+# NOTE: Dit weghalen bij het inleveren, aparte "main.py" file maken
+if __name__ == "__main__":
+    # Game('gameboards/Rushhour6x6_1.csv')
+    pass
