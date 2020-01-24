afa88b3fc5a1f2c9058e9f7e0f27f0f8fad9c2aa
Lulu Kroon <lulukroon@gmail.com>
2020-01-24 12:23:36 +0100

    karimi

--- code/algorithms/randomize.py
+++ code/algorithms/randomize.py
@@ -7,24 +7,28 @@ from code.classes.board import Board
 def randomize(board):
 
     cars = board.cars
+    current_board = board.board
 
     steps_dict = {}
     steps_count = 1
 
-    while board.won() == False:
+    while board.won(current_board) == False:
         # generate random car + move
         move = random.choice([-2,-1,1,2])
         car = random.choice(cars)
 
 
+
         move_car = board.move_car(cars, car, move)
-        move_car
 
         # increase steps if move is valid
         if move_car is not False:
-
-            steps_dict[steps_count] = [car[0],move], board.board
+            cars = move_car[0]
+            current_board = move_car[1]
+            board.print_board(cars, current_board)
+            steps_dict[steps_count] = [car[0],move], current_board
             steps_count += 1
+
 
     print(f"GAME IS WON IN {steps_count} STEPS!")
     return steps_dict
